ede5534c19486b448ee9d951c7d1eb01bd808205
Dan Rusnac <rusnac14@gmail.com>
2024-12-13 15:56:43 +0100

    style: navbar

--- apps/frontend/src/components/Navbar/Navbar.tsx
+++ apps/frontend/src/components/Navbar/Navbar.tsx
@@ -1,38 +1,37 @@
-import { HStack, Heading, Image, useBreakpointValue } from "@chakra-ui/react";
+import {
+  Box,
+  HStack,
+  Heading,
+  Image,
+  Stack,
+  useBreakpointValue,
+} from "@chakra-ui/react";
 import { WalletButton } from "@vechain/dapp-kit-react";
 import logo from "../../assets/logo.png";
 
 export const Navbar = () => {
   return (
-    <HStack justify={"space-between"} p={2} borderBottom={"1px solid #EEEEEE"}>
-      <HStack spacing={2}>
-        <Image
-          src={logo}
-          alt="logo"
-          w={"50px"}
-          // h="400px"
-          rounded="full"
-        />
-        <Heading size={"sm"}>VeChain Smart Accounts Factory</Heading>
-      </HStack>
+    <Stack
+      direction={"row"}
+      justify={"center"}
+      w={"full"}
+      borderBottom={"1px solid #EEEEEE"}
+    >
+      <HStack justify={"space-between"} p={2} maxW={"1000px"} w={"full"}>
+        <HStack spacing={2}>
+          <Image src={logo} alt="logo" w={"50px"} rounded="full" />
+          <Heading size={"sm"}>VeChain Smart Accounts Factory</Heading>
+        </HStack>
 
-      <HStack spacing={4}>
-        {/* {account && (
-          <Tag
-            size={"lg"}
-            variant="solid"
-            colorScheme={isMainnet ? "teal" : "cyan"}
-          >
-            {config.network.name.toUpperCase()}
-          </Tag>
-        )} */}
-        <WalletButton
-          mobile={useBreakpointValue({
-            base: true,
-            md: false,
-          })}
-        />
+        <HStack spacing={4}>
+          <WalletButton
+            mobile={useBreakpointValue({
+              base: true,
+              md: false,
+            })}
+          />
+        </HStack>
       </HStack>
-    </HStack>
+    </Stack>
   );
 };
